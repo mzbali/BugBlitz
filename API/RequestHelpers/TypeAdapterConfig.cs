bf9602b35c7e45cda18c0e67f10a61ad4671771c
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2023-11-16 19:24:37 +0000

    mapping config for dto conversion by mapster

--- API/RequestHelpers/TypeAdapterConfig.cs
+++ API/RequestHelpers/TypeAdapterConfig.cs
@@ -6,12 +6,20 @@ namespace API.RequestHelpers;
 
 public static class TypeAdapterConfig
 {
-    // Mapping each User object to UserDto, whenever User is being mapped to UserDto.
+    // Mapping each User object to MemberDto, whenever User is being mapped to MemberDtoMemberDto.
 
     public static void Configure()
     {
-        TypeAdapterConfig<User, UserDto>.NewConfig()
+        TypeAdapterConfig<User, MemberDto>.NewConfig()
         .Map(dest => dest.Id, src => src.Id)
         .Map(dest => dest.Username, src => src.UserName);
+        
+        TypeAdapterConfig<Member, MemberDto>.NewConfig()
+        .Map(dest => dest.Id, src => src.User.Id)
+        .Map(dest => dest.Username, src => src.User.UserName);
+
+        TypeAdapterConfig<Project, ProjectDto>.NewConfig()
+        .Map(dest => dest.Members, src => src.Members.Adapt<List<MemberDto>>());
+
     }
 }
