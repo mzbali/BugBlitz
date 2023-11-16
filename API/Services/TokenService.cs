39f4aaac711dd32de97076683844de84069b4274
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2023-11-16 19:10:01 +0000

    added: token secret

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -5,49 +5,48 @@ using API.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
-namespace API.Services
+namespace API.Services;
+
+public class TokenService
 {
-    public class TokenService
+    private readonly IConfiguration _configuration;
+    private readonly UserManager<User> _userManager;
+
+    public TokenService(UserManager<User> userManager, IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
-        private readonly UserManager<User> _userManager;
+        _userManager = userManager;
+        _configuration = configuration;
+    }
 
-        public TokenService(UserManager<User> userManager, IConfiguration configuration)
+    public async Task<string> GenerateTokenAsync(User user)
+    {
+        // Claims about the user
+        var claims = new List<Claim>
         {
-            _userManager = userManager;
-            _configuration = configuration;
-        }
+            new(ClaimTypes.Name, user.UserName),
+            new(ClaimTypes.Email, user.Email),
+        };
 
-        public async Task<string> GenerateToken(User user)
-        {
-            // Claims about the user
-            var claims = new List<Claim>
-            {
-                new(ClaimTypes.Name, user.UserName),
-                new(ClaimTypes.Email, user.Email)
-            };
+        var roles = await _userManager.GetRolesAsync(user);
 
-            var roles = await _userManager.GetRolesAsync(user);
+        // Roles that user claims
+        claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
 
-            // Roles that user claims
-            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+        // Making the key that would be used for signing
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTSettings:SecurityKey"]));
 
-            // Making the key that would be used for signing
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTSettings:SecurityKey"]));
+        // signing credentials it's the third part of the JwtSettings
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-            // signing credentials it's the third part of the JwtSettings
-            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        // Making the key
+        var options = new JwtSecurityToken(
+        null,
+        null,
+        claims,
+        expires: DateTime.Now.AddDays(7),
+        signingCredentials: credentials
+        );
 
-            // Making the key
-            var options = new JwtSecurityToken(
-            null,
-            null,
-            claims,
-            expires: DateTime.Now.AddDays(7),
-            signingCredentials: credentials
-            );
-
-            return new JwtSecurityTokenHandler().WriteToken(options);
-        }
+        return new JwtSecurityTokenHandler().WriteToken(options);
     }
 }
