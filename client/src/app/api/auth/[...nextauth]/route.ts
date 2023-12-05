fe0f1256066c3ce9d0eb1d62719467e7ecfbcc0c
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2023-12-05 00:05:42 +0000

    feat: use next-auth with keycloak

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,67 @@
-import type { NextAuthOptions } from 'next-auth';
-import NextAuth from 'next-auth';
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import { type JWT } from 'next-auth/jwt';
+import { type OAuthConfig } from 'next-auth/providers';
+import KeycloakProvider, {
+  type KeycloakProfile,
+} from 'next-auth/providers/keycloak';
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id_token?: string;
+    provider?: string;
+  }
+}
+
+declare module 'next-auth' {
+  interface Session {
+    accessToken?: string;
+  }
+}
 
 export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: 'jwt',
-  },
-  // Configure one or more authentication providers
   providers: [
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
-    // }),
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_ID || 'keycloak_client_id',
+      clientSecret: process.env.KEYCLOAK_SECRET || 'keycloak_client_secret',
+      issuer: process.env.KEYCLOAK_ISSUER || 'keycloak_url',
+    }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.id_token = account.id_token;
+        token.provider = account.provider;
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session) {
+        session = Object.assign({}, session, {
+          accessToken: token.accessToken,
+        });
+      }
+      return session;
+    },
+  },
+  events: {
+    async signOut({ token }: { token: JWT }) {
+      if (token.provider === 'keycloak') {
+        const issuerUrl = (
+          authOptions.providers.find(
+            (p) => p.id === 'keycloak',
+          ) as OAuthConfig<KeycloakProfile>
+        ).options!.issuer!;
+        const logOutUrl = new URL(
+          `${issuerUrl}/protocol/openid-connect/logout`,
+        );
+        logOutUrl.searchParams.set('id_token_hint', token.id_token!);
+        await fetch(logOutUrl);
+      }
+    },
+  },
 };
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
