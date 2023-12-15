8f2ac4c596212ff5690a79526c728693be842f9d
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2023-12-15 10:23:39 +0000

    feat: add types for models

---
+++ client/src/models/types.ts
@@ -0,0 +1,58 @@
+export interface Member {
+  username: string;
+}
+
+export interface User extends Member {
+  fullName: string;
+}
+
+export interface BugInputDto {
+  title: string;
+  description: string;
+  priority: number;
+}
+
+export interface Bug extends BugInputDto {
+  id: number;
+}
+
+export interface Note {
+  id: number;
+  bugId: number;
+  body: string;
+  createdAt: string;
+  updatedAt: string;
+  author: Member;
+}
+
+export interface ProjectInput {
+  name: string;
+  members: Member[];
+}
+
+export interface Project {
+  id: number;
+  name: string;
+  createdBy: Member;
+  members: Member[];
+  bugs: Bug[];
+  createdAt: string;
+}
+
+export interface BugDetails {
+  id: number;
+  projectId: number;
+  title: string;
+  description: string;
+  priority: string;
+  isResolved: boolean;
+  createdAt: string;
+  updatedAt: string | null;
+  closedAt: string | null;
+  reopenedAt: string | null;
+  createdBy: Member;
+  updatedBy: Member | null;
+  closedBy: Member | null;
+  reopenedBy: Member | null;
+  notes: Note[];
+}
