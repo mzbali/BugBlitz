ebeaeff525de6b886f0690b57828d9a390939a25
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2023-12-20 00:56:38 +0000

    feat: add additional properties to BugDto

--- API/DTOs/BugDto.cs
+++ API/DTOs/BugDto.cs
@@ -1,7 +1,18 @@
 namespace API.DTOs;
 
-public record BugDto: BugInputDto
+public record BugDto : BugInputDto
 {
     public int Id { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime? UpdatedAt { get; init; }
+
+    public MemberDto CreatedBy { get; init; }
+    public MemberDto? UpdatedBy { get; init; }
+
+    public List<NoteDto> Note { get; init; }
+
+    public int ProjectId { get; init; }
+
+    public bool IsResolved { get; init; }
 
 }
