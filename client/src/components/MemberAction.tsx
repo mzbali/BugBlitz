1dc5636e7f7fb323f3291b4b12f64b582bae0af3
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2024-01-12 21:44:15 +0000

    feat: add MemberAction component

---
+++ client/src/components/MemberAction.tsx
@@ -0,0 +1,70 @@
+'use client';
+import { DialogClose } from '@radix-ui/react-dialog';
+import { UserRoundPlus } from 'lucide-react';
+import React from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+
+import ComboBox from '@/components/ComboBox';
+import Button from '@/components/ui/buttons/Button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
+import { updateProject } from '@/app/actions';
+import { Project } from '@/models/types';
+
+interface Props {
+  project: Project;
+}
+
+const MemberAction: React.FC<Props> = ({ project }) => {
+  const [loading, setLoading] = React.useState(false);
+  const methods = useForm({
+    defaultValues: {
+      selectedMembers: project ? project.members.map((m) => m.username) : [],
+    },
+  });
+  const submitHandler = async (data: { selectedMembers: string[] }) => {
+    setLoading(true);
+    console.log(data.selectedMembers);
+    await updateProject(project.id, { members: data.selectedMembers });
+    setLoading(false);
+  };
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button leftIcon={UserRoundPlus}>Add Members</Button>
+      </DialogTrigger>
+      <DialogContent className='sm:max-w-[425px] dark:border-none dark:bg-slate-700'>
+        <DialogHeader>
+          <DialogTitle className='dark:text-white'>
+            Update Project Members
+          </DialogTitle>
+          <DialogDescription>
+            Update the member of this project.
+          </DialogDescription>
+        </DialogHeader>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(submitHandler)}>
+            <ComboBox name='selectedMembers' />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button isLoading={loading} type='submit' className='mt-2'>
+                  Save
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </form>
+        </FormProvider>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default MemberAction;
