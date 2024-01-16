2d4bd6028864113324edd7e18182f3c29c17f6b0
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2024-01-16 04:02:54 +0000

    refactor: formatDate function to include relative time option

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -16,14 +16,33 @@ export const getInitials = (name: string) => {
   return initials;
 };
 
-export const formatDate = (updatedAt: string | null): string => {
-  return updatedAt
-    ? new Intl.DateTimeFormat('default', {
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-      }).format(new Date(updatedAt))
-    : 'n/a';
+export const formatDate = (
+  updatedAt: string | null,
+  relativeTime?: boolean,
+): string => {
+  if (!updatedAt) return 'n/a';
+
+  const now = Date.now();
+  const updated = new Date(updatedAt).getTime();
+  const diffInMinutes = Math.round((now - updated) / 1000 / 60);
+
+  if (relativeTime) {
+    const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
+
+    if (diffInMinutes < 60) {
+      return rtf.format(-diffInMinutes, 'minute');
+    } else if (diffInMinutes < 60 * 24) {
+      return rtf.format(-Math.round(diffInMinutes / 60), 'hour');
+    } else {
+      return rtf.format(-Math.round(diffInMinutes / 60 / 24), 'day');
+    }
+  } else {
+    return new Intl.DateTimeFormat('default', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+    }).format(new Date(updatedAt));
+  }
 };
