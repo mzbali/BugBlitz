a3dd476fcc73c8179d6f7eb0ba03e8892c013b64
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2024-01-19 21:07:52 +0000

    feat: add copy credential for signin

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
+import AuthCheck from '@/components/auth/AuthCheck';
 import Container from '@/components/Container';
+import Copytext from '@/components/Copytext';
 import ArrowLink from '@/components/ui/links/ArrowLink';
 import ButtonLink from '@/components/ui/links/ButtonLink';
 import UnderlineLink from '@/components/ui/links/UnderlineLink';
@@ -9,7 +11,7 @@ import Logo from '~/svg/BB.svg';
 
 const HomePage = async () => {
   return (
-    <Container className='items-center justify-center'>
+    <Container className='mb-23 items-center justify-center'>
       <Logo className='w-16' />
       <h1 className='mt-4 text-gray-800 dark:text-white'>BugBlitz</h1>
       <p className='mt-2 text-sm text-dark  dark:text-gray-200'>
@@ -21,12 +23,18 @@ const HomePage = async () => {
           See the repository
         </ArrowLink>
       </p>
-      <ButtonLink className='mt-6' href='/components' variant='light'>
-        See all components
-      </ButtonLink>
       <ButtonLink className='mt-2' href='/projects' variant='primary'>
         See all Projects
       </ButtonLink>
+      <AuthCheck justCheck inverse>
+        <h4 className='mt-4'>
+          Click the signIn button to login with the credentials below.
+        </h4>
+        <div className='m-2 flex flex-col space-y-2'>
+          <Copytext field='Username' text='admin' />
+          <Copytext field='Password' text='Pa$$w0rd' />
+        </div>
+      </AuthCheck>
       <footer className='absolute bottom-2 text-gray-700'>
         © {new Date().getFullYear()} By{' '}
         <UnderlineLink href='https://mzbali.com'>Mahmud Bali</UnderlineLink>
