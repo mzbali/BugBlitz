18fe3aa06754da548135c3dd2f29eff3f0649b78
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2024-01-20 00:15:48 +0000

    fix: inputField styling

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -27,7 +27,7 @@ const InputField = ({
       name={id}
       id={id}
       placeholder={placeholder}
-      className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm'
+      className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 sm:text-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500'
       required={required}
     />
   </div>
