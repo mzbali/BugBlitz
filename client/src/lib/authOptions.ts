caea4fab598fff8f2aa83946afe555f5d24a1326
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2024-01-22 19:23:09 +0000

    fix: redirect functionality to authOptions

--- client/src/lib/authOptions.ts
+++ client/src/lib/authOptions.ts
@@ -91,6 +91,9 @@ export const authOptions: NextAuthOptions = {
       }
       return session;
     },
+    async redirect({ url: _, baseUrl }) {
+      return baseUrl;
+    },
   },
   events: {
     async signOut({ token }: { token: JWT }) {
