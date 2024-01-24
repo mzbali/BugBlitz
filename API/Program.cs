76f4b2088545805b94e438dc7065b32efaa7cb85
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2024-01-24 21:08:08 +0000

    fix: no swagger in production

--- API/Program.cs
+++ API/Program.cs
@@ -110,7 +110,7 @@ catch (Exception ex)
 }
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
